58e1c33bcff7907c7cf5d8aac150ce72cc230f1e
Seidko <64234553+Seidko@users.noreply.github.com>
2023-03-24 18:43:34 +0800

    feat(magical-commander): basic feature

--- packages/magical-commander/src/index.ts
+++ packages/magical-commander/src/index.ts
@@ -1,21 +1,31 @@
-import { Schema, Context } from 'koishi'
+import { Schema, Context, segment } from 'koishi'
 import { } from '@seidko/gpt-core'
 import { } from '@koishijs/cache'
 
 export const using = ['gpt'] as const
 export const name = 'magical-commander'
+export interface Config {
+  commands: string[]
+}
+export const Config: Schema<Config> = Schema.object({
+  commands: Schema.array(Schema.string().role('textarea')).description('各指令的说明，将会提供给AI。').default([
+    '`help [command:string]` 具有参数`[command:string]`，使用介绍：`你是谁？`、`这是什么？`、`怎么使用？`、`帮助`、`这机器人怎么用啊？`返回`help`，`novelai怎么用？` 返回用户 `help novelai`，注意事项：**如果用户需要帮助，应当返回此指令**'
+  ])
+})
 
-export async function apply(ctx: Context) {
+export async function apply(ctx: Context, config: Config) {
   ctx.i18n.define('zh', require('./locales/zh-CN'))
 
-  const conv = await ctx.gpt.create(ctx.i18n.text('zh', ['magical-commander.initial-prompt'], [`
-  help 显示机器人帮助。
-  rryth <prompt:text> 指令别名：”AI绘画，人人有图画“，具有参数 prompt:text，为贪婪匹配的文本类型，你需要将可能的参数翻译为英语，然后替换掉<prompt:text>并返回。例如：“AI绘画1girl”，返回“rryth 1girl”。“帮我画一个女孩”，返回“rryth 1girl”
-  `]))
+  const conv = await ctx.gpt.create({
+    initialPrompts: segment.unescape(ctx.i18n.text('zh', ['magical-commander.initial-prompt'], [config.commands.join('\n')]))
+  })
 
   ctx.middleware(async (session, next) => {
-    const { latestMessage: { message } } = await conv.ask(session.elements.toString())
-    if (message === 'none') return next()
-    return session.execute(message, next)
+    if (!session.content.startsWith('！')) return next()
+    const { latestMessage: { message } } = await conv.ask(segment.unescape(
+      ctx.i18n.text('zh', ['magical-commander.using-prompt'], [session.content])
+    ))
+    const command = message.match(/`(.*)`/)?.[1] ?? message
+    await session.execute(command)
   })
 }
