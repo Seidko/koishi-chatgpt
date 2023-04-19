ec5422da6b486004164d406ac38c523decb2776e
Seidko <64234553+Seidko@users.noreply.github.com>
2023-04-19 20:00:20 +0800

    fix: some bug fixes

--- packages/gpt-chatbot/src/index.ts
+++ packages/gpt-chatbot/src/index.ts
@@ -4,35 +4,34 @@ import { } from '@koishijs/cache'
 
 declare module '@koishijs/cache' {
   interface Tables {
-    'ai-chatbot/conv': string
+    'gpt-chatbot/conv': string
   }
 }
 
-export const using = ['llm', 'cache'] as const
-export const name = 'ai-chatbot'
+export const using = ['llm', '__cache__'] as const
+export const name = 'gpt-chatbot'
 
 export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export async function apply(ctx: Context, config: Config) {
-  const cache = ctx.cache('ai-chatbot/conv')
+  const cache = ctx.cache('gpt-chatbot/conv')
   const convId = await cache.get('conversation-id')
   let conv: Conversation
 
   ctx.command('ai.chat <prompt:text>')
     .action(async ({ session }, prompt) => {
-      conv = convId ? await ctx.llm.query(convId) : await ctx.llm.create()
+      if (!conv) conv = convId ? await ctx.llm.query(convId) : await ctx.llm.create()
 
       const newConv = await conv.ask(prompt, await cache.get(session.uid))
-      const { id, message } = newConv.messages[newConv.latestId]
-      await cache.set(session.uid, id)
-      return message
+      await cache.set(session.uid, newConv.latestId)
+      return newConv.renderElement()
     })
 
   ctx.command('ai.ask <prompt:text>')
     .action(async ({}, prompt) => {
-      conv = convId ? await ctx.llm.query(convId) : await ctx.llm.create()
-      return conv.ask(prompt).then(c => c.messages[c.latestId].message)
+      if (!conv) conv = convId ? await ctx.llm.query(convId) : await ctx.llm.create()
+      return conv.ask(prompt).then(c => c.renderElement())
     })
 
   ctx.command('ai.clear')
